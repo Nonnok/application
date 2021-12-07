3f0d4ae00b33b835da6cfd13e2e7bbaba73676be
Nonnok <donntenn0917@icloud.com>
2021-12-07 09:26:33 +0900

    Corrected the description of css

--- resources/views/layouts/nonlogin.blade.php
+++ resources/views/layouts/nonlogin.blade.php
@@ -5,10 +5,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
+  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/regist.css') }}">
   <title>@yield('title')</title>
-  <link rel="stylesheet" type="text/css" href="{{ mix('css/reset.css') }}">
-  <link rel="stylesheet" href="{{ mix('css/regist.css') }}">
 </head>
 <body>
   <header>
